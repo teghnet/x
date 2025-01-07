c06fa67432bbf015c1bd34d8e6b66c3c2416e516
Paweł Zaremba <pawzar@gmail.com>
2025-01-07 03:02:37 +0100

    Add Revolut parsing

---
+++ ops/bytes.go
@@ -0,0 +1,13 @@
+package ops
+
+import (
+	"crypto/sha256"
+	"strings"
+)
+
+var zeroByte = string([]byte{0})
+
+func Hash(line []string) []byte {
+	h := sha256.Sum256([]byte(strings.Join(line, zeroByte)))
+	return h[:16]
+}

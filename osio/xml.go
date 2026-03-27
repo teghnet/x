2264cd2c192248b9e1a43fc807448bd00ef33cec
Paweł Zaremba <pawzar@gmail.com>
2026-03-27 18:43:58 +0100

    Remove deprecated XML command files and clean up `go.mod`.

--- osio/xml.go
+++ osio/xml.go
@@ -11,11 +11,32 @@ import (
 	"html"
 	"io"
 	"iter"
-	"log"
 	"log/slog"
 	"regexp"
 	"strings"
+
+	"charm.land/log/v2"
 )
+
+func XMLDicts(r io.Reader) iter.Seq2[string, string] {
+	var xpath []string
+	return func(yield func(string, string) bool) {
+		for t := range tokens(xml.NewDecoder(r)) {
+			switch e := t.(type) {
+			case xml.StartElement:
+				xpath = append(xpath, e.Name.Local)
+			case xml.EndElement:
+				xpath = xpath[:len(xpath)-1]
+			case xml.CharData:
+				if !yield(strings.Join(xpath, " > "), string(e)) {
+					return
+				}
+			default:
+				log.Printf("%T", e)
+			}
+		}
+	}
+}
 
 func TrimXML(r io.Reader, w io.Writer, asHTML bool) (err error) {
 	prevElemType := ""
@@ -31,24 +52,24 @@ func TrimXML(r io.Reader, w io.Writer, asHTML bool) (err error) {
 		case xml.ProcInst:
 			_, err = fmt.Fprintf(w, "<?%s %s?>\n", el.Target, el.Inst)
 		case xml.Directive:
-			_, err = fmt.Fprintf(w, "!%s\n", el)
+			_, err = fmt.Fprintf(w, "<!%s>\n", el)
 		case xml.Comment:
 			_, err = fmt.Fprintf(w, "<!--%s-->\n", el)
 		case xml.StartElement:
-			// render start element
+			// render start-element
 			if prevElemType == "xml.StartElement" || prevElemType == "xml.EndElement" {
 				if _, err = fmt.Fprint(w, "\n", strings.Repeat("\t", len(xpath))); err != nil {
 					break
 				}
 			}
-			_, err = fmt.Fprint(w, startElement(el, len(xpath), strings.TrimSpace, html.EscapeString, NormalizeSpaces)...)
+			_, err = fmt.Fprint(w, startElement(el, "\n"+strings.Repeat("\t", len(xpath)), strings.TrimSpace, html.EscapeString, NormalizeSpaces)...)
 
 			// increase nesting level
 			xpath = append(xpath, el.Name.Local)
 		case xml.EndElement:
 			// decrease nesting level
 			xpath = xpath[:len(xpath)-1]
-			// render end element
+			// render end-element
 			if prevElemType == "xml.StartElement" || prevElemType == "xml.EndElement" {
 				if _, err = fmt.Fprint(w, "\n", strings.Repeat("\t", len(xpath))); err != nil {
 					break
@@ -71,33 +92,54 @@ func TrimXML(r io.Reader, w io.Writer, asHTML bool) (err error) {
 	return nil
 }
 
-var reSpaces = regexp.MustCompile(`\s+`)
-
-func NormalizeSpaces(input string) string {
-	return strings.TrimSpace(reSpaces.ReplaceAllString(input, " "))
-}
-
-func XMLDicts(r io.Reader) iter.Seq2[string, string] {
+func WriteElements(element string, r io.Reader, w io.Writer) (err error) {
+	split := strings.Split(element, "/")
+	name := split[len(split)-1]
+	var enabled bool
 	var xpath []string
-	return func(yield func(string, string) bool) {
-		for t := range tokens(xml.NewDecoder(r)) {
-			switch e := t.(type) {
-			case xml.StartElement:
-				xpath = append(xpath, e.Name.Local)
-			case xml.EndElement:
-				xpath = xpath[:len(xpath)-1]
-			case xml.CharData:
-				if !yield(strings.Join(xpath, " > "), string(e)) {
-					return
-				}
-			default:
-				log.Printf("%T", e)
+	for t := range tokens(xml.NewDecoder(r)) {
+		switch el := t.(type) {
+		case xml.ProcInst:
+			_, err = fPrintF(enabled, w, "<?%s %s?>", el.Target, el.Inst)
+		case xml.Directive:
+			_, err = fPrintF(enabled, w, "<!%s>", el)
+		case xml.Comment:
+			_, err = fPrintF(enabled, w, "<!--%s-->", el)
+		case xml.CharData:
+			err = escapeText(enabled, w, bytes.TrimSpace(reSpaces.ReplaceAll(el, []byte(" "))))
+		case xml.StartElement:
+			// start capturing
+			if el.Name.Local == name && strings.HasSuffix(strings.Join(append(xpath, el.Name.Local), "/"), element) {
+				enabled = true
 			}
+			// render
+			_, err = fPrintF(enabled, w, startElement(el, " ", strings.TrimSpace, html.EscapeString, NormalizeSpaces)...)
+			// increase nesting level
+			xpath = append(xpath, el.Name.Local)
+		case xml.EndElement:
+			// decrease nesting level
+			xpath = xpath[:len(xpath)-1]
+			// render
+			if el.Name.Space != "" {
+				el.Name.Space += ":"
+			}
+			_, err = fPrintF(enabled, w, []any{"</", el.Name.Space, el.Name.Local, ">"}...)
+			// stop capturing
+			if el.Name.Local == name && strings.HasSuffix(strings.Join(append(xpath, el.Name.Local), "/"), element) {
+				_, err = fPrintF(enabled, w, "\n")
+				enabled = false
+			}
+		default:
+			log.Printf("%T", el)
+		}
+		if err != nil {
+			return err
 		}
 	}
+	return nil
 }
 
-func startElement(el xml.StartElement, level int, fns ...func(string) string) []any {
+func startElement(el xml.StartElement, spacing string, fns ...func(string) string) []any {
 	if el.Name.Space != "" {
 		el.Name.Space += ":"
 	}
@@ -109,9 +151,29 @@ func startElement(el xml.StartElement, level int, fns ...func(string) string) []
 		for _, fn := range fns {
 			a.Value = fn(a.Value)
 		}
-		ret = append(ret, "\n", strings.Repeat("\t", level), a.Name.Space, a.Name.Local, `="`, a.Value, `"`)
+		ret = append(ret, spacing, a.Name.Space, a.Name.Local, `="`, a.Value, `"`)
 	}
 	return append(ret, ">")
+}
+
+func escapeText(enabled bool, w io.Writer, s []byte) error {
+	if !enabled {
+		return nil
+	}
+	return xml.EscapeText(w, s)
+}
+
+func fPrintF(enabled bool, w io.Writer, a ...any) (n int, err error) {
+	if !enabled {
+		return 0, nil
+	}
+	return fmt.Fprint(w, a...)
+}
+
+var reSpaces = regexp.MustCompile(`\s+`)
+
+func NormalizeSpaces(input string) string {
+	return strings.TrimSpace(reSpaces.ReplaceAllString(input, " "))
 }
 
 // tokens
